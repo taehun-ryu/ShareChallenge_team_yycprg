ce2c4a8edc25c8637db47e908a99f94c5c6499e0
taehun-ryu <xogns2079@gmail.com>
2023-01-27 14:51:17 +0900

    testtest2.py에서 data.decode() 조건 변경

--- test/testtest2.py
+++ test/testtest2.py
@@ -1,5 +1,6 @@
 import socket
 from _thread import *
+import time
 
 HOST = '127.0.0.1'
 PORT = 8000
@@ -13,10 +14,9 @@ def recv_data(client_socket) :
     while True :
         global data
         data = client_socket.recv(1024)
-        if 'LL' in data.decode():
+        if 'LL' in data.decode() or 'RR' in data.decode():
             continue
         print("recive : ",repr(data.decode()))
-        
 start_new_thread(recv_data, (client_socket,))
 print ('>> Connect Server')
 
